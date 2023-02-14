9f7bfcd0d9e8810ad704d656d55ee019de135beb
Kynytz <mauricioribeiro618@gmail.com>
2023-02-14 20:48:03 -0300

    Login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+    );
+  }
+}
