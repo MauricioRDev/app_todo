b6669e1757c6626e88f14b8ca9301aee4c114d36
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-02-15 17:17:39 -0300

    creation of list_page

---
+++ lib/pages/list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class List_Page extends StatefulWidget {
+  const List_Page({super.key});
+
+  @override
+  State<List_Page> createState() => _List_PageState();
+}
+
+class _List_PageState extends State<List_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
