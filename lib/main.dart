a830a3387aabcdf12cb3c5cdd523bf7acebd5c7d
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2023-02-17 19:19:35 -0300

    update list_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_todo/pages/list_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'test',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: ListaTarefas(),
+        )
     );
   }
 }
